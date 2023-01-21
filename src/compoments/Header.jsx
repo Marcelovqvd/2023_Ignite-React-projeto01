14e6a325fc96b733d9eb3b2010bb5cc48c1e8856
Marcelo <marcelovqvd@gmail.com>
2023-01-21 11:06:14 -0300

    Header

--- src/compoments/Header.jsx
+++ src/compoments/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
     return (
-        <div className={ styles.header }>
-            <h1>Header</h1>
-        </div>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logo" />
+        </header>
     )
 }
