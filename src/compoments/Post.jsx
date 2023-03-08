fed188a3b707a10d4992263d772e30d881351bc8
Marcelo <marcelovqvd@gmail.com>
2023-03-08 00:44:53 -0300

    aula: Formulário de comentários

--- src/compoments/Post.jsx
+++ src/compoments/Post.jsx
@@ -5,7 +5,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/Marcelovqvd.png" />
+          <img className={styles.avatar} src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
@@ -25,6 +25,18 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
