74b585a27ac57779654f500f4fded6fbe024fe60
Marcelo <marcelovqvd@gmail.com>
2023-06-14 16:53:40 -0300

    aula - Componente: Comment

--- src/compoments/Post.jsx
+++ src/compoments/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -13,30 +14,3 @@ export function Post() {
         </div>
 
         <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
-      </header>
-
-      <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
-      </div>
-
-      <form className={styles.commentForm}>
-        <strong>Deixe seu feedback</strong>
-
-        <textarea
-          placeholder="Deixe um comentário"
-        />
-
-        <footer>
-          <button type="submit">Publicar</button>
-        </footer>
-      </form>
-    </article>
-  )
-}
