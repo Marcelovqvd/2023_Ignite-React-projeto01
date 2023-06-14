b09c08c4417992530befbba7e056e515b6b51f56
Marcelo <marcelovqvd@gmail.com>
2023-06-14 15:07:13 -0300

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { Header } from './compoments/Header'
-
 import styles from './App.module.css'
 import './global.css';
-import { Sidebar } from './compoments/Sidebar';
-import { Post } from './compoments/Post';
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <Post />
-      </div>
-    </>
+        <div className={styles.wrapper}>
+          <div className={styles.div1}>sidebar</div>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti velit iure ad aliquam! Non, nobis porro unde voluptates fugit aliquam rerum ut recusandae fuga odit incidunt itaque eum amet beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores accusantium repudiandae dignissimos, velit tenetur quae dolor mollitia! Vel molestias possimus sapiente aliquid quod aliquam blanditiis, modi, iusto a, hic culpa. Lorem ipsum dolor sit amet consectetur, adipisicing elit. A hic fugiat in vitae, optio odit laboriosam consequuntur repellat sit fuga placeat itaque beatae commodi ea quo animi nisi debitis officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, totam! Nobis, dignissimos quasi beatae molestias molestiae maxime optio, vero labore quos animi velit blanditiis quas repellat, aliquid consequuntur doloremque quam.</div>
+        </div>
+      
   )
 }
 
