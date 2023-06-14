b9a6b862001d50fa2ac24f2a1d2939abad5453c6
Marcelo <marcelovqvd@gmail.com>
2023-06-14 17:35:41 -0300

    aula - Componente: Avatar

--- src/compoments/Sidebar.jsx
+++ src/compoments/Sidebar.jsx
@@ -1,26 +1,29 @@
-import styles from './Sidebar.module.css'
-import {  PencilLine } from 'phosphor-react'
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
 
-    return (
-        <aside className={styles.sidebar}>
-            <img 
-                className={styles.cover} 
-                src="https://images.unsplash.com/photo-1674231313303-ab9bd1196390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=50" 
-            />
-            <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Marcelovqvd.png" />
-                <strong>Marcelo</strong>
-                <span>Web Developer</span>
-            </div>
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/maykbrito.png" />
 
-            <footer>
-                <a href="#">
-                    <PencilLine size={20}/>
-                    Editar seu perfil
-                </a>
-            </footer>
-        </aside>
-    )
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
