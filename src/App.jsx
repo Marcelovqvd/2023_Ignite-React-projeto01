f361a1d7dc1c926cb652db6271ffd65542dae128
Marcelo <marcelovqvd@gmail.com>
2023-06-20 09:38:02 -0300

    initial settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import styles from './App.module.css'
-import './global.css';
+import { useState } from "react"
+import { Comment } from "./Comment"
 
 function App() {
+  
   return (
-        <div className={styles.wrapper}>
-          <div className={styles.div1}>sidebar</div>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti velit iure ad aliquam! Non, nobis porro unde voluptates fugit aliquam rerum ut recusandae fuga odit incidunt itaque eum amet beatae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores accusantium repudiandae dignissimos, velit tenetur quae dolor mollitia! Vel molestias possimus sapiente aliquid quod aliquam blanditiis, modi, iusto a, hic culpa. Lorem ipsum dolor sit amet consectetur, adipisicing elit. A hic fugiat in vitae, optio odit laboriosam consequuntur repellat sit fuga placeat itaque beatae commodi ea quo animi nisi debitis officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, totam! Nobis, dignissimos quasi beatae molestias molestiae maxime optio, vero labore quos animi velit blanditiis quas repellat, aliquid consequuntur doloremque quam.</div>
-        </div>
+    <div>
       
+    </div>
   )
 }
 
