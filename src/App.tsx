7f8192a1b46e585a41d63597c8e301e741cb8112
Marcelo <marcelovqvd@gmail.com>
2023-12-12 10:14:46 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,31 @@
-import { Header } from './components/Header';
-import { Post, PostType } from './components/Post'
-import { Sidebar } from './components/Sidebar';
+import { Header } from "./components/Header"
+import { Post, PostType } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 
-import styles from './App.module.css';
+import styles from "./App.module.css"
 
-import './global.css';
+import "./global.css"
 
 const posts: PostType[] = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://github.com/diego3g.png',
-      name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
+      avatarUrl: "https://github.com/marcelovqvd.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
+    publishedAt: new Date("2022-05-03 20:00:00"),
   },
-  {
-    id: 2,
-    author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
-  },
-];
+]
 
 export function App() {
   return (
@@ -45,13 +35,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {posts.map(post => {
-            return (
-              <Post
-                key={post.id}
-                post={post}
-              />
-            )
+          {posts.map((post) => {
+            return <Post key={post.id} post={post} />
           })}
         </main>
       </div>
